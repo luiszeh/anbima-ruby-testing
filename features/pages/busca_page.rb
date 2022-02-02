3d7b65ccb736605c744e3344c9ab97a216012a5c
Fonseca Gorgulho <l.fonseca.gorgulho@accenture.com>
2022-02-02 18:59:22 -0300

    steps estruturado

--- features/pages/busca_page.rb
+++ features/pages/busca_page.rb
@@ -7,24 +7,25 @@ element :input_isin, '.anbima-ui-input-search-clean'
 element :input_emissor, '.anbima-ui-input-search-clean'
 element :input_agente, '.anbima-ui-input-search-clean'
 element :input_cnpj, '.anbima-ui-input-search-clean'
+element :busca_sucedida, '.title-normal'
 
-def busca_b3
+def busca_b3()
     input_b3.send_keys("AALM11"), :return
 end
 
-def busca_isin
+def busca_isin()
     input_isin.send_keys("BRAALMDBS009"), :return
 end
 
-def busca_emissor
+def busca_emissor()
     input_emissor.send_keys("AURA ALMAS MINERACAO S.A"), :return
 end
 
-def busca_agente
+def busca_agente()
     input_agente.send_keys("OLIVEIRA TRUST DTVM S/A"), :return
 end
 
-def busca_cnpj
+def busca_cnpj()
     input_cnpj.send_keys("08213823000107"), :return
 end
 
---
+++ features/step_definitions/busca_steps.rb
@@ -0,0 +1,44 @@
+Dado('que estou na página de busca por Debêntures') do
+    @busca_page = Busca.new
+    @busca_page.load
+end
+
+Quando('insiro o Código B3 no campo de busca') do
+  @busca_page.busca_b3()
+end
+
+Quando('insiro o código ISIN no campo de busca') do
+  @busca_page.busca_isin()
+end
+
+Quando('insiro o nome do Emissor no campo de busca') do
+  @busca_page.busca_emissor()   
+end
+
+Quando('insiro o nome do Agente no campo de busca') do
+  @busca_page.busca_agente() 
+end
+
+Quando('insiro o CNPJ no campo de busca') do
+  @busca_page.busca_cnpj()
+end
+
+Então ('encontro o ativo pelo Código B3') do
+  expect(@busca_page.busca_sucedida).to have_content("Debêntures (1)")
+end
+
+Então ('encontro o ativo pelo ISIN') do
+  expect(@busca_page.busca_sucedida).to have_content("Debêntures (1)")
+end
+
+Então ('encontro o ativo pelo nome do Emissor') do
+  expect(@busca_page.busca_sucedida).to have_content("Debêntures (1)")   
+end
+
+Então ('encontro o ativo pelo nome do Agente') do
+  expect(@busca_page.busca_sucedida).to have_content("Debêntures (1)")   
+end
+
+Então ('encontro o ativo pelo CNPJ') do
+  expect(@busca_page.busca_sucedida).to have_content("Debêntures (1)")  
+end
